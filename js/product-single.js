6da8ad446954ce42911144c6ca5d18937f85dca6
keiyin <keiyin6261@gmail.com>
2021-11-15 01:11:44 +0800

    add ajax to single product

--- js/product-single.js
+++ js/product-single.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var token = "secret_lJj3ZfB62VLsgEgvKIHy7sWzhL8Wo9PEUWVnSRcwYUc";
+
     $.urlParam = function(name){
         var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
         if (results==null){
@@ -8,4 +10,32 @@ $(document).ready(function(){
            return results[1] || 0;
         }
     }
+    var name = $.urlParam("name");
+    var data = {
+        "filter":{
+            "property":"Name",
+            "text":{
+                "equals":name
+            }
+        }
+    };
+    
+    $.ajax({
+        type: "POST",
+        url: "https://pickkabook.tk/notionBestSeller",
+        headers: {
+            "Authorization": "Bearer "+token,
+            "Notion-Version": "2021-08-16"
+        },
+        data:JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(data){
+            alert(data);
+        },
+        error: function(data){
+            console.log(data.statusText);
+        },
+        cache:"false"
+    })
 });
