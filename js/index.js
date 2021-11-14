34ce1874a39bb4ec5d8ba21a7cb974de034f7073
keiyin <keiyin6261@gmail.com>
2021-11-14 23:11:40 +0800

    added dynamic

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     //alert("hello");
-    var databaseId = "9737653f-da86-44b1-968e-8bcaf8be06ba";
     var token = "secret_lJj3ZfB62VLsgEgvKIHy7sWzhL8Wo9PEUWVnSRcwYUc";
 
     $.ajax({
@@ -13,7 +12,36 @@ $(document).ready(function(){
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function(data){
-            alert(data);
+            var parameterList = JSON.parse(escapeHtml(JSON.stringify(data.result)));
+            $.each(	parameterList,function() {
+                var html = "<div class='col-md-4'>" + 
+				"<div class='product-item'>" +
+					"<div class='product-thumb'>" +
+						"<img class='img-responsive' src='images/shop/products/product-1.jpg' alt='product-img' />"+
+						"<div class='preview-meta'>" +
+							"<ul>" +
+								"<li>" +
+									"<span  data-toggle='modal' data-target='#product-modal'>" +
+										"<i class='tf-ion-ios-search-strong'></i>" +
+									"</span>" +
+								"</li>" +
+								"<li>" +
+			                        "<a href='#!' ><i class='tf-ion-ios-heart'></i></a>" +
+								"</li>" +
+								"<li>" +
+									"<a href='#!'><i class='tf-ion-android-cart'></i></a>" +
+								"</li>"+
+							"</ul>" +
+                      	"</div>" +
+					"</div>" +
+					"<div class='product-content'>" +
+						"<h4><a href='product-single.html?name=123'>Hello World</a></h4>" +
+						"<p class='price'>$200</p>" +
+					"</div>" +
+				"</div>" +
+			"</div>";
+                $("#bestSellerItems").append(html);
+            })
         },
         error: function(data){
             alert(data.statusText);
@@ -22,7 +50,4 @@ $(document).ready(function(){
     })
 
 
-
-
-    
 });
